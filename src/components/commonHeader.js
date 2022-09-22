dc1b6aaabda7fbc30b08b99633d3750c64d9c415
Muntaeyeon <monty420420@naver.com>
2022-09-22 23:52:54 +0900

    logo

--- src/components/commonHeader.js
+++ src/components/commonHeader.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import './css/commonHeader.scss';
+import { Link } from 'react-router-dom';
 
 function CommonHeader(props) {
     return (
 <header id="CommonHeader" className='header'>
   <section className='header_container'>
     <div className='container'>
-    <div className='container_logo'>hi</div>
+      <img className="container_logo" src="img/adidas_logo.png" alt="main logo"  />
+
     <div className='container_menu'>
         <div className='menu'>
-            <div className='menu_men'>men</div>
-            <div className='menu_women'>women</div>
-            <div className='menu_event'>event</div>
+            <div className='menu_gender'>
+            <Link className='menu_gender_men' to="/">MEN</Link>
+            <Link className='menu_gender_women' to="/">WOMEN</Link>
+            <Link className='menu_gender_kids' to="/">KIDS</Link>
+            </div>
+            <div className='menu_type'>
+            <Link className='menu_type_sports' to="/">SPORTS</Link>
+            <Link className='menu_type_brands' to="/">BRANDS</Link>
+            <Link className='menu_type_sale' to="/">SALE</Link>
+            </div>
         </div>
     </div>
     </div>
